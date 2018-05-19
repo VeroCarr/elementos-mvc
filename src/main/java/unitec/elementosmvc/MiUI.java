012d8ee85afcf7812b89245b51da59d5b836ad13
T-107 <T-107@PC185.mshome.net>
2018-05-18 21:19:54 -0500

    Primera Version

---
+++ src/main/java/unitec/elementosmvc/MiUI.java
@@ -0,0 +1,40 @@
+package unitec.elementosmvc;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+/**
+ *
+ * @author T-107
+ */
+@SpringUI
+public class MiUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+        VerticalLayout layout = new VerticalLayout();
+
+        Label etiqueta = new Label("Bienvenido a mi aplicacion");
+
+        etiqueta.addStyleName(ValoTheme.LABEL_H2);
+
+        Button boton = new Button("Ingresar");
+
+        boton.addStyleName(ValoTheme.BUTTON_DANGER);
+
+        //agregamos la etiqueta y el boton a el layout
+        layout.addComponent(etiqueta);
+        layout.addComponent(boton);
+
+        //finalmente agregamos el layout al contenedor principal de init
+        setContent(layout);
+
+    }
+
+}
