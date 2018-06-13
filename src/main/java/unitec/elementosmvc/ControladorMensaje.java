435a002a2846326f16f7ac427cb0b0d439ada581
T-107 <T-107@PC184.mshome.net>
2018-06-12 21:42:45 -0500

    controlers

---
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -0,0 +1,17 @@
+
+package unitec.elementosmvc;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") //Aplication Package interface
+@CrossOrigin
+public class ControladorMensaje {
+    @GetMapping("/hola")
+    public String algo(){
+        return "Hola mundo desde un controlsdor";
+    }
+}
