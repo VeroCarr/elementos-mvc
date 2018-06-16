42840a2e9b2fc8c4df54d723676a76d6852641bd
T-107 <T-107@PC184.mshome.net>
2018-06-15 21:40:41 -0500

    Reglas de servicio red para controladores

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -1,8 +1,12 @@
 
 package unitec.elementosmvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api") //Aplication Package interface
 @CrossOrigin
 public class ControladorMensaje {
-    @GetMapping("/hola")
-    public String algo(){
-        return "Hola mundo desde un controlsdor";
+    @Autowired RepositorioMensaje repoMensaje;
+    //INCISO A:: BUSCAR TODOS
+    @GetMapping("/mensaje")
+    public List<Mensaje> buscarTodos(){
+        return  repoMensaje.findAll();
+    }
+    //CASO B: BUSCAR POR ID
+    
+    @GetMapping("/mensaje/{id}") //conocido como variable de ruta
+    public Mensaje buscarporId(@PathVariable String id){
+        Mensaje mensa=new Mensaje();
+        mensa.setId(id);
+        return repoMensaje.findById(id).get();
     }
 }
