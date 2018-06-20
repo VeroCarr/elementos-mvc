7d946ee2c8d041631aecccb40f6827b05a8cef6e
T-107 <T-107@PC184.mshome.net>
2018-06-19 21:40:09 -0500

    guardar

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -1,12 +1,15 @@
 
 package unitec.elementosmvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +31,18 @@ public class ControladorMensaje {
         mensa.setId(id);
         return repoMensaje.findById(id).get();
     }
+    //Caso C: Guardar
+    @PostMapping("/mensaje")
+    public Estatus guardar(@RequestBody String json) throws Exception{
+       //primero convertimos este String json a objeto java
+       ObjectMapper maper=new ObjectMapper();
+       Mensaje mensa=maper.readValue(json, Mensaje.class);
+       repoMensaje.save(mensa);
+       System.out.println("este objeto se convirtio "+mensa);
+        Estatus estatus=new Estatus();
+        estatus.setSuccess(true);
+        estatus.setMensaje("Mensaje guardado con exito");
+        return estatus;
+    }
+            
 }
